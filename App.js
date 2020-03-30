bec00fbed7d48344837297940f512296a56a6140
NeitanDev <natan.miranda02@gmail.com>
2020-03-29 21:23:40 -0300

    Colocar fonte no App, realizado com sucesso

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  View,
+} from 'react-native';
+import * as Font from 'expo-font';
+import Index from './src/index';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  componentDidMount() {
+    Font.loadAsync({
+      'ubuntu-regular': require('./assets/font//Ubuntu-Regular.ttf'),
+    });
+  }
+  render() {
+    return (
+      <Index/>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 56, fontFamily: 'ubuntu-regular' }}>
+          Sounou
+        </Text>
+      </View>
+  );
+}
